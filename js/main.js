13b2e2b8b409d550ffe3b00d98062292857d6f60
Mubashir Shahzad <ms.dew2552@gmail.com>
2022-07-25 07:55:20 +0500

    added icons scetion

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// -----------------search variables------------
+let searchForm=document.querySelector(".search-form");
+let searchButton=document.querySelector("#search-btn");
+
+// -----------login variables---------
+let loginForm=document.querySelector(".login-form");
+let loginCloseBtn=document.querySelector("#close-login-btn");
+let loginButton=document.querySelector("#login-btn");
+
+
+
+// -------------now java-----------
+// -------------search form-----------
+searchButton.onclick=()=>{
+    searchForm.classList.toggle("active")
+}
+
+
+// -------------login form-----------
+loginButton.onclick=()=>{
+    loginForm.classList.add("active")
+}
+loginCloseBtn.onclick=()=>{
+    loginForm.classList.remove("active")
+}
+// ---------------swipper area------------
+// ---------------swipper area------------
+var swiper = new Swiper(".book-slider", {
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
